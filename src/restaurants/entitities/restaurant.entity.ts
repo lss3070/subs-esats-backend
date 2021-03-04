d7e8cc65b47bd6d643c5f2cff34b7f76c70e6965
songseop.lee <lss3070@hanmail.net>
2021-03-04 21:34:26 +0900

    typeORM && postgres update

--- src/restaurants/entitities/restaurant.entity.ts
+++ src/restaurants/entitities/restaurant.entity.ts
@@ -1,17 +1,29 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity() //decorator
 export class Restaurant {
-  @Field((type) => {
-    return String;
-  })
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
+  @Field((type) => String)
+  @Column()
   name: string;
   @Field((type) => Boolean, { nullable: true })
+  @Column()
   isVegan?: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownerName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryNmae: string;
 }
