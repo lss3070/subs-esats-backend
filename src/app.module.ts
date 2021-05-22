5bfd0e12b87e975f763b99ab2a233b00508db614
songseop.lee <lss3070@hanmail.net>
2021-05-22 17:16:12 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,9 +33,9 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(), //Joi는 환경변수 유효성검사
+        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(), //Joi는 환경변수 유효성검사
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
